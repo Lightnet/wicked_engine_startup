1a4ff2d718f316c22009ee47620297c34039772d
Lightnet <darkneter@gmail.com>
2024-04-24 22:20:05 -0700

    Update game.lua

--- game.lua
+++ game.lua
@@ -3,23 +3,21 @@
 local scene = GetScene()
 
 runProcess(function()
+  SetProfilerEnabled(false) -- have a bit more screen space
   scene.Clear()
   ClearWorld()
-  SetProfilerEnabled(false) -- have a bit more screen space
   LoadModel(script_dir() .. "assets/level.wiscene")
   local path = RenderPath3D()
   application.SetActivePath(path)
-
-  SetProfilerEnabled(false) -- have a bit more screen space
 
   cam_entity = scene.Entity_FindByName("camera") -- query the from scene
   cam_component = scene.Component_GetCamera(cam_entity)
   transform = scene.Component_GetTransform(cam_entity) -- get camera's transform
   GetCamera().TransformCamera(transform) -- Transform the main camera with transform that you got from camera in the scene
 
-  backlog_post("/////////////////////////////////////");
-  backlog_post("///",os.time());
-  backlog_post("/////////////////////////////////////");
+  -- backlog_post("/////////////////////////////////////");
+  -- backlog_post("///",os.time());
+  -- backlog_post("/////////////////////////////////////");
   --local camera = GetCamera()
   while true do
     --local camera = GetCamera()
@@ -27,13 +25,12 @@ runProcess(function()
     update()
     --backlog_post("///",os.time());
     if(not backlog_isactive() and input.Press(string.byte('R'))) then
-			-- reload script
-			backlog_post("RELOAD", os.time())
-			killProcesses()
-			--application.SetActivePath(prevPath)
+      -- reload script
+      backlog_post("RELOAD", os.time())
+      killProcesses()
+      --application.SetActivePath(prevPath)
       dofile(script_dir() .. "game.lua")
-			return
+      return
 		end
   end
-
 end)
