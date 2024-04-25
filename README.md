7a56c14b30f20ce9566edfb707e569a759cee1f4
Lightnet <darkneter@gmail.com>
2024-04-24 21:04:06 -0700

    Added

---
+++ README.md
@@ -0,0 +1,15 @@
+# wicked_engine_startup
+
+# licnese: MIT
+
+# Created By: Lightnet
+
+# Game Engine:
+ * WickedEngine - https://github.com/turanszkij/WickedEngine
+
+# Information:
+  Simple startup.lua to run without editor.
+
+  The setup scene example for camera, cube and direct lighting. With the camera rotate off a bit.
+
+  Took time to get it working. Docs example for WickedEngine.
---
+++ game.lua
@@ -0,0 +1,39 @@
+
+
+local scene = GetScene()
+
+runProcess(function()
+  scene.Clear()
+  ClearWorld()
+  SetProfilerEnabled(false) -- have a bit more screen space
+  LoadModel(script_dir() .. "assets/level.wiscene")
+  local path = RenderPath3D()
+  application.SetActivePath(path)
+
+  SetProfilerEnabled(false) -- have a bit more screen space
+
+  cam_entity = scene.Entity_FindByName("camera") -- query the from scene
+  cam_component = scene.Component_GetCamera(cam_entity)
+  transform = scene.Component_GetTransform(cam_entity) -- get camera's transform
+  GetCamera().TransformCamera(transform) -- Transform the main camera with transform that you got from camera in the scene
+
+  backlog_post("/////////////////////////////////////");
+  backlog_post("///",os.time());
+  backlog_post("/////////////////////////////////////");
+  --local camera = GetCamera()
+  while true do
+    --local camera = GetCamera()
+    --camera.UpdateCamera()
+    update()
+    --backlog_post("///",os.time());
+    if(not backlog_isactive() and input.Press(string.byte('R'))) then
+			-- reload script
+			backlog_post("RELOAD", os.time())
+			killProcesses()
+			--application.SetActivePath(prevPath)
+      dofile(script_dir() .. "game.lua")
+			return
+		end
+  end
+
+end)
